4c0d3ad84605761a2e76a946f605b04f29118f83
shfwan <shafwan367@gmail.com>
2025-01-03 06:33:32 +0900

    penambahan feature untuk CRUD data

--- src/features/rental.js
+++ src/features/rental.js
@@ -1,3 +1,12 @@
+import useAxiosAuth from "@/hooks/useAxiosAuth"
+import { useMutation } from "@tanstack/react-query"
+
 export const usePostRental = ({onSuccess}) => {
-    return
+    const axiosAuth = useAxiosAuth()
+    return useMutation({
+        mutationFn: async (body) => {
+            return axiosAuth.post("/api/rental", body)
+        },
+        onSuccess
+    })
 }
